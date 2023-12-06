6b8d0dd43a364f514993558db74cbf889f4590f1
Silver-Blade333 <131536879+Silver-Blade333@users.noreply.github.com>
2023-12-06 23:32:30 +0530

    Update 1-D Running sum.py

--- Learning prototype/1-D Running sum.py
+++ Learning prototype/1-D Running sum.py
@@ -1,1 +1,13 @@
-
+a = [10, 3, 5]
+t = 0
+#print(len(a))
+if len(a) <= 1000:
+    for i in a:
+        if -10**6 <= i <= 10**6:
+            t = t + i
+            #print(t)
+        else:
+         continue
+else:
+    print("Please enter only 1000 elements")
+print(t)
